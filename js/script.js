ee9a06b98badfda8e5d6f71f15953eebc4a0e5cd
Jian Zhong <zhongjian089@gmail.com>
2022-12-03 05:31:28 -0800

    add mouseover effect on rolling banner

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 "use strict";
 
 // Enable videos on the main page to play automatically on mouse over, stop and resume on mouse out.
-const videos = document.getElementsByTagName('video');
+const videos = document.getElementsByClassName("videos index-page"); // Only for index page's videos
 
 for (let i = 0; i < videos.length; i++) {
-    videos[i].addEventListener('mouseover', function () {
-        this.play();    // play when mouse is hovering over
+    videos[i].addEventListener('mouseover', function () { // div tag
+        this.children[0].play();                          // play video tag
     });
+
     videos[i].addEventListener('mouseout', function () {
-        this.pause();         // stop playing when mouse is not hovering over
-        this.currentTime = 0; // resume video track time
+        this.children[0].pause();         // stop playing when mouse is not hovering over
+        this.children[0].currentTime = 0; // resume video track time
     });
 }
+
+
+// Pause the rolling banner animation when mouse over, continue to run while the mouse is out.
+const rollingBanner = document.getElementById('rollingBanner');
+
+rollingBanner.addEventListener('mouseover', function() {
+    rollingBanner.firstElementChild.style.animationPlayState = 'paused'; // pause animation
+    rollingBanner.style.cursor = "pointer";
+})
+
+rollingBanner.addEventListener('mouseout', function() {
+    rollingBanner.firstElementChild.style.animationPlayState = 'running'; // run animation
+})
