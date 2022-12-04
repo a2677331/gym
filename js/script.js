354b6879139d77f03cdc8ce59cb9c31e1c05aa33
Jian Zhong <zhongjian089@gmail.com>
2022-12-03 22:52:44 -0800

    fixed css 4 errors

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ if (window.innerWidth > 800) {
 
   for (let i = 0; i < videos.length; i++) {
     videos[i].addEventListener("mouseover", function () {
-
       this.children[0].play(); // play the video tag
 
       // const playPromise = this.children[0].play(); // play the video tag
@@ -35,5 +34,44 @@ if (window.innerWidth > 800) {
 
   rollingBanner.addEventListener("mouseout", function () {
     rollingBanner.firstElementChild.style.animationPlayState = "running"; // run animation
+  });
+
+  // show background images when hovering over muscle buttons, and disappear when mouse out.
+  const nav = document.getElementById("nav-container");
+
+  // For Chest button
+  const chestButton = document.getElementById("chest-button");
+  chestButton.children[0].addEventListener("mouseover", function () {
+    nav.style.backgroundImage = "url('images/chest.png')";
+  });
+  chestButton.children[0].addEventListener("mouseout", function () {
+    nav.style.backgroundImage = null;
+  });
+
+  // For Legs button
+  const legsButton = document.getElementById("legs-button");
+  legsButton.children[0].addEventListener("mouseover", function () {
+    nav.style.backgroundImage = "url('images/legs.png')";
+  });
+  legsButton.children[0].addEventListener("mouseout", function () {
+    nav.style.backgroundImage = null;
+  });
+
+  // For Shoulders button
+  const shouldersButton = document.getElementById("shoulders-button");
+  shouldersButton.children[0].addEventListener("mouseover", function () {
+    nav.style.backgroundImage = "url('images/shoulder.png')";
+  });
+  shouldersButton.children[0].addEventListener("mouseout", function () {
+    nav.style.backgroundImage = null;
+  });
+
+  // For Back button
+  const backButton = document.getElementById("back-button");
+  backButton.children[0].addEventListener("mouseover", function () {
+    nav.style.backgroundImage = "url('images/back.png')";
+  });
+  backButton.children[0].addEventListener("mouseout", function () {
+    nav.style.backgroundImage = null;
   });
 }
