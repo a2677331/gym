96f0eff2f67ce78e1d78108cec175b72b050aada
Jian Zhong <zhongjian089@gmail.com>
2022-12-03 21:47:56 -0800

    test play()

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@
 
 // Only execute if tablet mode and desktop mode
 if (window.innerWidth > 800) {
-
   // Enable videos on the main page to play automatically on mouse over, stop and resume on mouse out.
   const videos = document.getElementsByClassName("videos index-page"); // Only for index page's videos
 
   for (let i = 0; i < videos.length; i++) {
-    videos[i].addEventListener("mouseover", function () { 
+    videos[i].addEventListener("mouseover", function () {
+
       this.children[0].play(); // play the video tag
+
+      // const playPromise = this.children[0].play(); // play the video tag
+      // if (playPromise !== null) {
+      //   playPromise.catch(() => {
+      //     this.children[0].play();
+      //   });
+      // }
+      
     });
 
     videos[i].addEventListener("mouseout", function () {
