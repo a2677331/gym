00f50e11db17ef3767c6083c59b31efd027ae13c
Jian Zhong <zhongjian089@gmail.com>
2022-12-03 17:55:22 -0800

    Removed rolling banner motion under mobile view, and add autoplay for music

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 "use strict";
 
-// Enable videos on the main page to play automatically on mouse over, stop and resume on mouse out.
-const videos = document.getElementsByClassName("videos index-page"); // Only for index page's videos
+// Only execute if tablet mode and desktop mode
+if (window.innerWidth > 800) {
 
-for (let i = 0; i < videos.length; i++) {
-    videos[i].addEventListener('mouseover', function () { // div tag
-        this.children[0].play();                          // play video tag
+  // Enable videos on the main page to play automatically on mouse over, stop and resume on mouse out.
+  const videos = document.getElementsByClassName("videos index-page"); // Only for index page's videos
+
+  for (let i = 0; i < videos.length; i++) {
+    videos[i].addEventListener("mouseover", function () { 
+      this.children[0].play(); // play the video tag
     });
 
-    videos[i].addEventListener('mouseout', function () {
-        this.children[0].pause();         // stop playing when mouse is not hovering over
-        this.children[0].currentTime = 0; // resume video track time
+    videos[i].addEventListener("mouseout", function () {
+      this.children[0].pause(); // stop playing when mouse is not hovering over
+      this.children[0].currentTime = 0; // resume video track time
     });
-}
+  }
 
+  // Pause the rolling banner when mouse is hovering over, resume to run while the mouse is out.
+  const rollingBanner = document.getElementById("rollingBanner");
 
-// Pause the rolling banner animation when mouse over, continue to run while the mouse is out.
-const rollingBanner = document.getElementById('rollingBanner');
-
-rollingBanner.addEventListener('mouseover', function() {
-    rollingBanner.firstElementChild.style.animationPlayState = 'paused'; // pause animation
+  rollingBanner.addEventListener("mouseover", function () {
+    rollingBanner.firstElementChild.style.animationPlayState = "paused"; // pause animation
     rollingBanner.style.cursor = "pointer";
-})
+  });
 
-rollingBanner.addEventListener('mouseout', function() {
-    rollingBanner.firstElementChild.style.animationPlayState = 'running'; // run animation
-})
+  rollingBanner.addEventListener("mouseout", function () {
+    rollingBanner.firstElementChild.style.animationPlayState = "running"; // run animation
+  });
+}
