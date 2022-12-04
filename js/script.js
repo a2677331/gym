cc98741ad9cf981d110ccb4a3449c127f5d812f9
Jian Zhong <zhongjian089@gmail.com>
2022-12-04 00:51:37 -0800

    fixed play() error finally

--- js/script.js
+++ js/script.js
@@ -4,35 +4,37 @@
 if (window.innerWidth > 800) {
 
   /**** Handle Video tags interaction ****/
-  const videos = document.getElementsByClassName("videos index-page");
-  let isPlaying = false; // make sure pause() does not interupt play() request
 
-  // handle video play on mouse over
-  for (let i = 0; i < videos.length; i++) { // For each video tag
+  const videos = document.getElementsByClassName("videos index-page");
+
+  // Handle video play on mouse over,
+  // for each video tag, add mouseout and mouseover events
+  for (let i = 0; i < videos.length; i++) {
     videos[i].addEventListener("mouseover", function () {
 
-      // get video status
-      isPlaying =
-        this.children[0].currentTime > 0 &&
-        !this.children[0].paused &&
-        !this.children[0].ended &&
-        this.children[0].readyState > this.children[0].HAVE_CURRENT_DATA;
-
-      // play video
-      this.children[0].play(); 
+      // Best practices for video.play() returning a promise
+      const playPromise = this.children[0].play();
+      if (playPromise !== undefined) {
+        playPromise
+          .then(function () {
+            // Automatic playback started!
+          })
+          .catch(function (error) {
+            // Automatic playback failed.
+            // can let the console print out info why.
+          });
+      }
     });
 
     // handle video stop and resume event on mouse out
     videos[i].addEventListener("mouseout", function () {
-      if (isPlaying) {
-        this.children[0].pause(); // stop playing when mouse is not hovering over
-        this.children[0].currentTime = 0; // resume video track time
-      }
+      this.children[0].pause(); // stop playing when mouse is not hovering over
+      this.children[0].currentTime = 0; // resume video track time
     });
   }
 
-
   /**** Handle Rolling Banner interaction ****/
+
   const rollingBanner = document.getElementById("rollingBanner");
 
   rollingBanner.addEventListener("mouseover", function () {
@@ -44,8 +46,8 @@ if (window.innerWidth > 800) {
     rollingBanner.firstElementChild.style.animationPlayState = "running"; // run animation
   });
 
-
   /**** Handle Button backgroundImage interaction ****/
+
   const nav = document.getElementById("nav-container");
 
   // For Chest button
